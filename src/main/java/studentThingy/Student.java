3380a8f5f335711592a692a97f800e29cdadffe4
Rolin_em <ahmed007rayyan@gmail.com>
2026-09-21 12:13:49 +0500

    Add files via upload

---
+++ src/main/java/studentThingy/Student.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package information;
+
+public class Student {
+    String name;
+    int index;
+    Student(String name, int index){
+        this.name = name;
+        this.index = index;
+            
+   
+    }
+}
